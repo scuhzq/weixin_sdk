29a7e3e438978ace405b2dc49f6b9cd710250c01
usydapeng <usydapeng@163.com>
2016-02-29 16:15:08 +0800

    init

---
+++ src/main/java/com/oldpeng/Application.java
@@ -0,0 +1,11 @@
+package com.oldpeng;
+
+/**
+ * Created by dapeng on 16/2/29.
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
