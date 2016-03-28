10628ee1cda29f4fb16016386b07fef1fce487e5
usydapeng <usydapeng@163.com>
2016-03-28 13:27:05 +0800

    运算工具类

---
+++ src/main/java/com/oldpeng/core/utils/MathUtils.java
@@ -0,0 +1,69 @@
+package com.oldpeng.core.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+
+/**
+ * Created by dapeng on 16/3/28.
+ */
+public class MathUtils {
+
+	public static <T> T multiply(double price, int multiples, Class<T> clazz){
+		BigDecimal bd1 = new BigDecimal(Double.toString(price));
+		BigDecimal bd2 = new BigDecimal(Double.toString(multiples));
+		try {
+			return convert2Number(bd1.multiply(bd2), clazz);
+		} catch (Throwable t) {
+			throw new RuntimeException(t.getMessage(), t);
+		}
+	}
+
+	public static <T> T divide(Number number, Number multiples, Class<T> clazz){
+		BigDecimal bd1 = new BigDecimal(number.toString());
+		BigDecimal bd2 = new BigDecimal(multiples.toString());
+		try {
+			return convert2Number(bd1.divide(bd2), clazz);
+		} catch (Throwable t) {
+			throw new RuntimeException(t.getMessage(), t);
+		}
+	}
+
+	public static <T> T subtract(Number minuend, Number subtractor, Class<T> clazz){
+		BigDecimal bd1 = new BigDecimal(minuend.toString());
+		BigDecimal bd2 = new BigDecimal(subtractor.toString());
+		try {
+			return convert2Number(bd1.subtract(bd2), clazz);
+		} catch (Throwable t) {
+			throw new RuntimeException(t.getMessage(), t);
+		}
+	}
+
+	public static <T> T add(Number addend1, Number addend2, Class<T> clazz){
+		BigDecimal bd1 = new BigDecimal(addend1.toString());
+		BigDecimal bd2 = new BigDecimal(addend2.toString());
+		try {
+			return convert2Number(bd1.add(bd2), clazz);
+		} catch (Throwable t) {
+			throw new RuntimeException(t.getMessage(), t);
+		}
+	}
+
+	private static  <T> T convert2Number(BigDecimal bigDecimal, Class<T> clazz) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		if(clazz.equals(Integer.class)){
+			Method method = clazz.getMethod("valueOf", int.class);
+			return (T) method.invoke(null, bigDecimal.intValue());
+		} else if(clazz.equals(Double.class)){
+			Method method = clazz.getMethod("valueOf", double.class);
+			return (T) method.invoke(null, bigDecimal.doubleValue());
+		} else if(clazz.equals(Long.class)){
+			Method method = clazz.getMethod("valueOf", long.class);
+			return (T) method.invoke(null, bigDecimal.longValue());
+		} else if(clazz.equals(Float.class)){
+			Method method = clazz.getMethod("valueOf", float.class);
+			return (T) method.invoke(null, bigDecimal.floatValue());
+		} else {
+			throw new RuntimeException("class is error");
+		}
+	}
+}
